d4ae3451d16dc3e5e6a66a6d871b37af69cca8ef
Ken Williams <zoo9000@hotmail.com>
2013-02-19 12:45:09 +0000

    Initial commit of source file and test page.

---
+++ src/libs/kwilliams/mt4/MT4Pips.java
@@ -0,0 +1,14 @@
+package libs.kwilliams.mt4;
+
+public class MT4Pips {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
